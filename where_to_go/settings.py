3b5dab37ab6e55705387f8a84ecd1b1dda8b206a
LuFent <vladmarchenkosteam@gmail.com>
2022-04-05 18:24:30 +0300

    lufent.pythonanywhere.com added

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1', 'lufent.pythonanywhere.com']
 
 
 # Application definition
