8ddcf2644a630a607220a54467d97499502eb11e
LuFent <vladmarchenkosteam@gmail.com>
2022-04-06 03:06:26 +0300

    static fixed for pythonanywhere

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -112,9 +112,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
 
 
